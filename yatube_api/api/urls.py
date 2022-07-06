c709298d6e2eaeac5f4835144165b8e59ea9b533
Aleksei Viunik <vjunik88@gmail.com>
2022-07-06 21:03:47 +0300

    Sprint 8 is done. Round 2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,13 +2,14 @@ from rest_framework.authtoken import views
 from rest_framework import routers
 from django.urls import include, path
 
-from .views import CommentViewSet, GroupViewSet, PostViewSet
+from .views import CommentViewSet, GroupViewSet, PostViewSet, UserViewSet
 
 app_name = 'api'
 
 v1_router = routers.DefaultRouter()
-v1_router.register(r'posts', PostViewSet)
-v1_router.register(r'groups', GroupViewSet)
+v1_router.register('posts', PostViewSet)
+v1_router.register('groups', GroupViewSet)
+v1_router.register('users', UserViewSet)
 v1_router.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
--- yatube_api/posts/serializers.py
+++ yatube_api/posts/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'cats')
+        fields = ('id', 'username', 'posts')
         ref_name = 'ReadOnlyUsers'
 
 
